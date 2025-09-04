a4a319aee8667e5ca3ca309c2901eaf2a7eae0b0
anshdadhich <anshdadhichmm@gmail.com>
2025-09-04 22:21:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -256,7 +256,7 @@ export default function Home() {
             fetchStats();
           }}
           variant="outline"
-          className="bg-black text-white px-3 py-2 rounded-lg border-2 border-transparent hover:bg-white hover:text-black hover:border-black shadow-md transition-all duration-300 transform hover:scale-102"
+          className="bg-black text-white px-3 py-2 rounded-lg border-2 border-transparent hover:bg-white hover:text-black hover:border-black shadow-md transition-all duration-300 transform hover:scale-102 active:scale-95""
         >
           Refresh
         </Button>
